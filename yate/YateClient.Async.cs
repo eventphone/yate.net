d2c1393a607c0d7ead0f49e232a37d622519d2f5
Bianco Veigel <zivillian@eventphone.de>
2018-09-04 20:32:56 +0200

    refactoring

---
+++ yate/YateClient.Async.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace eventphone.yate
+{
+    public partial class YateClient
+    {
+        /// <summary>
+        /// As the conection is initiated from the external module the engine must be informed on the role of the connection.
+        /// This must be the first request sent over a newly established socket connection.
+        /// </summary>
+        /// <remarks>
+        /// The role and direction of the connection is established and then this keyword cannot be used again on the same connection.
+        /// There is no answer to this request - if it fails the engine will slam the connection shut.
+        /// </remarks>
+        public Task ConnectAsync(RoleType role, CancellationToken cancellationToken)
+        {
+            return ConnectAsync(role, null, null, cancellationToken);
+        }
+
+        /// <summary>
+        /// As the conection is initiated from the external module the engine must be informed on the role of the connection.
+        /// This must be the first request sent over a newly established socket connection.
+        /// </summary>
+        /// <remarks>
+        /// The role and direction of the connection is established and then this keyword cannot be used again on the same connection.
+        /// There is no answer to this request - if it fails the engine will slam the connection shut.
+        /// </remarks>
+        public Task ConnectAsync(RoleType role, string channelId, CancellationToken cancellationToken)
+        {
+            return ConnectAsync(role, channelId, null, cancellationToken);
+        }
+
+        /// <summary>
+        /// As the conection is initiated from the external module the engine must be informed on the role of the connection.
+        /// This must be the first request sent over a newly established socket connection.
+        /// </summary>
+        /// <remarks>
+        /// The role and direction of the connection is established and then this keyword cannot be used again on the same connection.
+        /// There is no answer to this request - if it fails the engine will slam the connection shut.
+        /// </remarks>
+        public async Task ConnectAsync(RoleType role, string channelId, string channelType, CancellationToken cancellationToken)
+        {
+            await _client.ConnectAsync(_host, _port).ConfigureAwait(false);
+            string roleType;
+            switch (role)
+            {
+                case RoleType.Global:
+                    roleType = "global";
+                    break;
+                case RoleType.Channel:
+                    roleType = "channel";
+                    break;
+                case RoleType.Play:
+                    roleType = "play";
+                    break;
+                case RoleType.Record:
+                    roleType = "record";
+                    break;
+                case RoleType.PlayRec:
+                    roleType = "playrec";
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(role));
+            }
+            _reader = new Thread(Read) {IsBackground = true, Name = "YateClientReader"};
+            _reader.Start();
+            await SendAsync(Command(Commands.SConnect, roleType, channelId, channelType), cancellationToken).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// used to relay arbitrary output messages to engine's logging output
+        /// </summary>
+        public Task LogAsync(string message, CancellationToken cancellationToken)
+        {
+            return SendAsync(Command(Commands.SOutput) + ':' + message, cancellationToken);
+        }
+
+        /// <summary>
+        /// query local parameter
+        /// </summary>
+        public Task<string> GetLocalAsync(string parameter, CancellationToken cancellationToken)
+        {
+            return SetLocalAsync(parameter, String.Empty, cancellationToken);
+        }
+
+        /// <summary>
+        /// requests the change of a local parameter
+        /// </summary>
+        public async Task<string> SetLocalAsync(string parameter, string value, CancellationToken cancellationToken)
+        {
+            var result = await SendAsync(Commands.RSetLocal, parameter, cancellationToken, Commands.SSetLocal, parameter, value).ConfigureAwait(false);
+            return _serializer.Decode(result[2]);
+        }
+
+        /// <summary>
+        /// sent by the application to the engine to ask it to process a message.
+        /// </summary>
+        /// <param name="name">name of the message</param>
+        /// <param name="result">default textual return value of the message</param>
+        /// <param name="parameter">enumeration of the key-value pairs of the message</param>
+        /// <returns></returns>
+        public async Task<YateMessageResponse> SendMessageAsync(string name, string result, CancellationToken cancellationToken, params Tuple<string, string>[] parameter)
+        {
+            string id = Guid.NewGuid().ToString();
+            var time = DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+            var stringParams = new[] {Commands.SMessage, id, time, name, result}.Concat(parameter.Select(x => _serializer.Encode(x)));
+            var response = await SendAsync(Commands.RMessage, id, cancellationToken, stringParams.ToArray()).ConfigureAwait(false);
+            var resultParams = new List<Tuple<string, string>>();
+            for (int i = 5; i < response.Length; i++)
+            {
+                resultParams.Add(_serializer.DecodeParameter(response[i]));
+            }
+            return new YateMessageResponse
+            {
+                Id = response[1],
+                Handled = "true".Equals(_serializer.Decode(response[2]), StringComparison.OrdinalIgnoreCase),
+                Name = _serializer.Decode(response[3]),
+                Result = _serializer.Decode(response[4]),
+                Parameter = resultParams
+            };
+        }
+
+        public async Task<T> SendMessageAsync<T>(IYateMessageResponse<T> message, CancellationToken cancellationToken)
+        {
+            var result = await SendMessageAsync(message.Name, message.Result, cancellationToken, message.Parameters.ToArray()).ConfigureAwait(false);
+            return message.ParseResponse(result, _serializer);
+        }
+
+        public Task<InstallResult> InstallAsync(string name, string filterName, CancellationToken cancellationToken)
+        {
+            return InstallAsync(null, name, filterName, null, cancellationToken);
+        }
+
+        public Task<InstallResult> InstallAsync(string name, CancellationToken cancellationToken)
+        {
+            return InstallAsync(null, name, null, null, cancellationToken);
+        }
+
+        public Task<InstallResult> InstallAsync(string name, string filterName, string filterValue, CancellationToken cancellationToken)
+        {
+            return InstallAsync(null, name, filterName, filterValue, cancellationToken);
+        }
+
+        public Task<InstallResult> InstallAsync(int priority, string name, string filterName, string filterValue, CancellationToken cancellationToken)
+        {
+            return InstallAsync((int?) priority, name, filterName, filterValue, cancellationToken);
+        }
+
+        public async Task<InstallResult> UninstallAsync(string name, CancellationToken cancellationToken)
+        {
+            var result = await SendAsync(Commands.RUninstall, name, cancellationToken, Commands.SUninstall, name).ConfigureAwait(false);
+            return new InstallResult(_serializer.Decode(result[1]), _serializer.Decode(result[3]));
+        }
+
+        public async Task<bool> WatchAsync(string name, CancellationToken cancellationToken)
+        {
+            var result = await SendAsync(Commands.RWatch, name, cancellationToken, Commands.SWatch, name).ConfigureAwait(false);
+            return "true".Equals(_serializer.Decode(result[2]), StringComparison.OrdinalIgnoreCase);
+        }
+
+        public async Task<bool> WatchAsync(string name, Action<YateMessageEventArgs> callback, CancellationToken cancellationToken)
+        {
+            var bag = _watchCallbacks.GetOrAdd(name, new ConcurrentBag<Action<YateMessageEventArgs>>());
+            bag.Add(callback);
+            var response = await SendAsync(Commands.RWatch, name, cancellationToken, Commands.SWatch, name).ConfigureAwait(false);
+            return "true".Equals(_serializer.Decode(response[2]), StringComparison.OrdinalIgnoreCase);
+        }
+
+        public async Task<bool> UnwatchAsync(string name, CancellationToken cancellationToken)
+        {
+            var result = await SendAsync(Commands.RUnwatch, name, cancellationToken, Commands.SUnwatch, name).ConfigureAwait(false);
+            return "true".Equals(_serializer.Decode(result[2]), StringComparison.OrdinalIgnoreCase);
+        }
+
+        private async Task<InstallResult> InstallAsync(int? priority, string name, string filterName, string filterValue, CancellationToken cancellationToken)
+        {
+            var result = await SendAsync(Commands.RInstall, name, cancellationToken, Commands.SInstall, priority?.ToString() ?? String.Empty, name, filterName, filterValue).ConfigureAwait(false);
+            return new InstallResult(_serializer.Decode(result[1]), _serializer.Decode(result[3]));
+        }
+
+        private async Task<string[]> SendAsync(string responseCommand, string key, CancellationToken cancellationToken, params string[] parameter)
+        {
+            var response = new YateResponse();
+            var eventKey = GetKey(responseCommand, key);
+            if (!_eventQueue.TryAdd(eventKey, response))
+                throw new ArgumentException("this command is currently pending");
+            await SendAsync(Command(parameter), cancellationToken).ConfigureAwait(false);
+            return await response.GetResponseAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        private async Task SendAsync(string message, CancellationToken cancellationToken)
+        {
+            var buffer = Encoding.UTF8.GetBytes(message + '\n');
+            await _writeLock.WaitAsync(cancellationToken).ConfigureAwait(false);
+            try
+            {
+                var stream = _client.GetStream();
+                await stream.WriteAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false);
+                await stream.FlushAsync(cancellationToken).ConfigureAwait(false);
+            }
+            finally
+            {
+                _writeLock.Release();
+            }
+        }
+    }
+}
---
+++ yate/YateClient.Receive.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace eventphone.yate
+{
+    public partial class YateClient
+    {
+        private void Read()
+        {
+            using (var reader = new StreamReader(_client.GetStream(), Encoding.UTF8))
+            while (!_isclosed)
+            {
+                try
+                {
+                    var response = reader.ReadLine();
+                    if (response == null) continue;
+                    var parts = response.Split(':');
+                    parts[0] = _serializer.Decode(parts[0]);
+                    switch (parts[0])
+                    {
+                        case Commands.RSetLocal:
+                            parts[1] = _serializer.Decode(parts[1]);
+                            ProcessResponse(Commands.RSetLocal, parts[1], parts);
+                            break;
+                        case Commands.RError:
+                            OnError(parts[1]);
+                            break;
+                        case Commands.RMessage:
+                            parts[1] = _serializer.Decode(parts[1]);
+                            if (!ProcessResponse(Commands.RMessage, parts[1], parts))
+                            {
+                                var message = GetYateMessageEventArgs(parts);
+                                OnWatch(message);
+                            }
+                            break;
+                        case Commands.SMessage:
+                            var arg = GetYateMessageEventArgs(parts);
+                            OnMessageReceived(arg);
+                            break;
+                        case Commands.RInstall:
+                            parts[2] = _serializer.Decode(parts[2]);
+                            ProcessResponse(Commands.RInstall, parts[2], parts);
+                            break;
+                        case Commands.RUninstall:
+                            parts[2] = _serializer.Decode(parts[2]);
+                            ProcessResponse(Commands.RUninstall, parts[2], parts);
+                            break;
+                        case Commands.RWatch:
+                            parts[1] = _serializer.Decode(parts[1]);
+                            ProcessResponse(Commands.RWatch, parts[1], parts);
+                            break;
+                        case Commands.RUnwatch:
+                            parts[1] = _serializer.Decode(parts[1]);
+                            ProcessResponse(Commands.RUnwatch, parts[1], parts);
+                            break;
+                        default:
+                            throw new NotImplementedException();
+                    }
+                }
+                catch (IOException ex)
+                {
+                    var socketExept = ex.InnerException as SocketException;
+                    if (socketExept == null || socketExept.ErrorCode != 10004)
+                        throw;
+                }
+            }
+        }
+
+        private YateMessageEventArgs GetYateMessageEventArgs(string[] parts)
+        {
+            var resultParams = new List<Tuple<string, string>>();
+            for (int i = 5; i < parts.Length; i++)
+            {
+                resultParams.Add(_serializer.DecodeParameter(parts[i]));
+            }
+            return new YateMessageEventArgs(_serializer.Decode(parts[1]), _serializer.Decode(parts[2]), _serializer.Decode(parts[3]), _serializer.Decode(parts[4]), resultParams);
+        }
+
+        protected virtual void OnMessageReceived(YateMessageEventArgs message)
+        {
+            var handler = MessageReceived;
+            try
+            {
+                handler?.Invoke(this, message);
+            }
+            catch (Exception ex)
+            {
+                LogAsync(ex.Message, CancellationToken.None).GetAwaiter().GetResult();
+            }
+            var commands = new List<string>
+            {
+                Commands.RMessage,
+                message.Id,
+                message.Handled ? "true" : "false",
+                message.Name,
+                message.Result
+            };
+            commands.AddRange(message.NewParameter.Select(x => _serializer.Encode(x)));
+            var response = Command(commands.ToArray());
+            SendAsync(response, CancellationToken.None).GetAwaiter().GetResult();
+        }
+
+        protected virtual void OnWatch(YateMessageEventArgs message)
+        {
+            var handler = Watch;
+            try
+            {
+                handler?.Invoke(this, message);
+            }
+            catch (Exception ex)
+            {
+                LogAsync(ex.Message, CancellationToken.None).GetAwaiter().GetResult();
+            }
+        }
+
+        private void InvokeWatchCallbacks(object sender, YateMessageEventArgs e)
+        {
+            if (_watchCallbacks.TryGetValue(e.Name, out var bag))
+            {
+                foreach (var callback in bag)
+                {
+                    callback(e);
+                }
+            }
+        }
+
+        public event EventHandler<YateMessageEventArgs> MessageReceived;
+        public event EventHandler<YateMessageEventArgs> Watch;
+
+        private bool ProcessResponse(string command, string key, string[] values)
+        {
+            if (_eventQueue.TryRemove(GetKey(command, key), out var response))
+            {
+                response.Values = values;
+                return true;
+            }
+            return false;
+        }
+
+        private void OnError(string error)
+        {
+            foreach (var value in _eventQueue.Values)
+            {
+                value.Error = error;
+            }
+        }
+
+        private class YateResponse : IDisposable
+        {
+            private readonly SemaphoreSlim _resetEvent;
+            private string[] _values;
+            private string _error;
+
+            public YateResponse()
+            {
+                _resetEvent = new SemaphoreSlim(0, 1);
+            }
+
+            public string[] Values
+            {
+                get { return _values; }
+                set
+                {
+                    _values = value;
+                    _resetEvent.Release();
+                }
+            }
+
+            public string Error
+            {
+                get { return _error; }
+                set
+                {
+                    _error = value;
+                    _resetEvent.Release();
+                }
+            }
+
+            public async Task<string[]> GetResponseAsync(CancellationToken cancellationToken)
+            {
+                await _resetEvent.WaitAsync(cancellationToken).ConfigureAwait(false);
+                if (_error != null)
+                    throw new YateException(Error);
+                return Values;
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+            }
+
+            private void Dispose(bool disposing)
+            {
+                if (disposing)
+                {
+                    _resetEvent?.Dispose();
+                }
+            }
+        }
+    }
+}
